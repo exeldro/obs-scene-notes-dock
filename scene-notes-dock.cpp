5dcf3e76e89b7cd473a801d44ceb5d58d15ad945
Exeldro <exelsor@hotmail.com>
2021-11-14 16:31:34 +0100

    add text format to context menu

--- scene-notes-dock.cpp
+++ scene-notes-dock.cpp
@@ -1,6 +1,10 @@
 #include "scene-notes-dock.hpp"
 #include <obs-module.h>
+#include <QColorDialog>
+#include <QFontDialog>
 #include <QMainWindow>
+#include <QMenu>
+#include <QStyle>
 #include <QVBoxLayout>
 
 #include "version.h"
@@ -47,17 +51,18 @@ static void frontend_event(enum obs_frontend_event event, void *data)
 		if (!scene)
 			return;
 
-		auto *settings = obs_source_get_settings(scene);
-		if (settings) {
-			auto *dock = static_cast<SceneNotesDock *>(data);
-			dock->textEdit->setHtml(QT_UTF8(obs_data_get_string(settings, "notes")));
+		if (auto *settings = obs_source_get_settings(scene)) {
+			const auto *dock = static_cast<SceneNotesDock *>(data);
+			dock->textEdit->setHtml(QT_UTF8(
+				obs_data_get_string(settings, "notes")));
 			obs_data_release(settings);
 		}
 		obs_source_release(scene);
 	}
 }
 
-SceneNotesDock::SceneNotesDock(QWidget *parent) : QDockWidget(parent)
+SceneNotesDock::SceneNotesDock(QWidget *parent)
+	: QDockWidget(parent), textEdit(new QTextEdit(this))
 {
 	setFeatures(DockWidgetMovable | DockWidgetFloatable);
 	setWindowTitle(QT_UTF8(obs_module_text("SceneNotes")));
@@ -66,8 +71,6 @@ SceneNotesDock::SceneNotesDock(QWidget *parent) : QDockWidget(parent)
 	hide();
 
 	auto *mainLayout = new QVBoxLayout(this);
-
-	textEdit = new QTextEdit(this);
 
 	mainLayout->addWidget(textEdit);
 
@@ -79,11 +82,10 @@ SceneNotesDock::SceneNotesDock(QWidget *parent) : QDockWidget(parent)
 		auto *scene = obs_frontend_get_current_scene();
 		if (!scene)
 			return;
-		auto *settings = obs_source_get_settings(scene);
-		if (settings) {
-			auto old_notes =
+		if (auto *settings = obs_source_get_settings(scene)) {
+			const auto old_notes =
 				QT_UTF8(obs_data_get_string(settings, "notes"));
-			auto new_notes = textEdit->toHtml();
+			const auto new_notes = textEdit->toHtml();
 			if (old_notes != new_notes) {
 				obs_data_set_string(settings, "notes",
 						    QT_TO_UTF8(new_notes));
@@ -93,6 +95,58 @@ SceneNotesDock::SceneNotesDock(QWidget *parent) : QDockWidget(parent)
 		obs_source_release(scene);
 	};
 	connect(textEdit, &QTextEdit::textChanged, changeText);
+
+	textEdit->setContextMenuPolicy(Qt::CustomContextMenu);
+	auto contextMenu = [this]() {
+		auto *menu = textEdit->createStandardContextMenu();
+		menu->addSeparator();
+		auto setFont = [this]() {
+			bool success = false;
+			const auto font = QFontDialog::getFont(
+				&success, textEdit->currentFont(), this,
+				QT_UTF8(obs_module_text("Font")));
+			if (success)
+				textEdit->setCurrentFont(font);
+		};
+		menu->addAction(QT_UTF8(obs_module_text("Font")), this,
+				setFont);
+
+		auto setFontColor = [this]() {
+			const QColor newColor = QColorDialog::getColor(
+				textEdit->textColor(), this,
+				QT_UTF8(obs_module_text("TextColor")));
+			if (newColor.isValid()) {
+				textEdit->setTextColor(newColor);
+			}
+		};
+		menu->addAction(QT_UTF8(obs_module_text("TextColor")), this,
+				setFontColor);
+		auto setBackgroundColor = [this]() {
+			const QColor newColor = QColorDialog::getColor(
+				textEdit->textBackgroundColor(), this,
+				QT_UTF8(obs_module_text("BackGroundColor")));
+			if (newColor.isValid()) {
+				textEdit->setTextBackgroundColor(newColor);
+			}
+		};
+		menu->addAction(QT_UTF8(obs_module_text("BackgroundColor")),
+				this, setBackgroundColor);
+		menu->addSeparator();
+		auto clearFormat = [this]() {
+			const auto text = textEdit->toPlainText();
+			textEdit->setTextColor(textEdit->palette().color(
+				QPalette::ColorRole::Foreground));
+			textEdit->setTextBackgroundColor(
+				textEdit->palette().color(
+					QPalette::ColorRole::Background));
+			textEdit->setCurrentFont(textEdit->font());
+			textEdit->setPlainText(text);
+		};
+		menu->addAction(QT_UTF8(obs_module_text("ClearFormat")), this,
+				clearFormat);
+		menu->exec(QCursor::pos());
+	};
+	connect(textEdit, &QTextEdit::customContextMenuRequested, contextMenu);
 
 	obs_frontend_add_event_callback(frontend_event, this);
 }
